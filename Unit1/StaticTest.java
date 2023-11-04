3b581f94f713e0bba830edee2622814d774f9d0b
Linuxe <angelren1220@gmail.com>
2023-11-04 16:29:07 -0400

    add codes

---
+++ Unit1/StaticTest.java
@@ -0,0 +1,19 @@
+package Unit1;
+
+public class StaticTest 
+ { 
+// Declaration of static variable. 
+    static int a = 20; 
+    void m1() 
+    { 
+      int a = 30; 
+      System.out.println("a: " +a); 
+      System.out.println("a: " +StaticTest.a); // Accessing static variable using class name within instance method. 
+}
+ public static void main(String[] args) 
+ { 
+    StaticTest st = new StaticTest(); 
+    st.m1(); 
+ } 
+}
+
