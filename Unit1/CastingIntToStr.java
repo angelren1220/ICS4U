e76e46020131e662fa3afda1b36648c535c87cb9
Linuxe <angelren1220@gmail.com>
2023-11-07 15:29:29 -0500

    add codes

--- Unit1/CastingIntToStr.java
+++ Unit1/CastingIntToStr.java
@@ -2,6 +2,7 @@
 public class CastingIntToStr {
   public static void main(String[] args){
     int num = 1234;
+    System.out.println("The integer is: " + num);
     String str = Integer.toString(num);
     System.out.println("The string is: " + str);
   }
---
+++ Unit1/CastingStrToInt.java
@@ -0,0 +1,8 @@
+public class CastingStrToInt {
+  public static void main(String[] args){
+    String str = "1234";
+    System.out.println("The string is: " + str);
+    int num = Integer.parseInt(str);
+    System.out.println("The integer is: " + num);
+  }
+}
