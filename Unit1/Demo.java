57cc05e1220e98bf5cbf5af577b69f7e607b74a9
Linuxe <angelren1220@gmail.com>
2023-11-07 16:31:30 -0500

    add codes

---
+++ Unit1/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+  public static void main(String[] args) {
+    int sum = 0;
+    int n = 50;
+    for(int i = 0; i <= n; i ++){
+      sum = sum + i;
+    }
+
+    System.out.println("The sum of " + n + " is " + sum);
+  }
+}
