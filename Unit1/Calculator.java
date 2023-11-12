128ce343ed7e9df5ab8b4101eaf259cbbcbdff65
Linuxe <angelren1220@gmail.com>
2023-11-11 21:50:11 -0500

    add validation

--- Unit1/Calculator.java
+++ Unit1/Calculator.java
@@ -11,56 +11,68 @@ public class Calculator {
     System.out.println("5. Remainder");
   }
 
-public static double addition(double num1, double num2){
-  return num1 + num2;
-}
+  public static double addition(double num1, double num2) {
+    return num1 + num2;
+  }
 
-public static double subtraction(double num1, double num2){
-  return num1 - num2;
-}
+  public static double subtraction(double num1, double num2) {
+    return num1 - num2;
+  }
 
-public static double multiplication(double num1, double num2){
-  return num1 * num2;
-}
+  public static double multiplication(double num1, double num2) {
+    return num1 * num2;
+  }
 
-public static double division(double num1, double num2){
-  return num1 / num2;
-}
+  public static double division(double num1, double num2) {
+    return num1 / num2;
+  }
 
-public static double remainder(double num1, double num2){
-  return num1 % num2;
-}
+  public static double remainder(double num1, double num2) {
+    return num1 % num2;
+  }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter number 1:");
     double num1 = sc.nextDouble();
-    System.out.println("Enter number 1:");
+    System.out.println("Enter number 2:");
     double num2 = sc.nextDouble();
 
     displayMenu();
     int op = sc.nextInt();
-    sc.close();
-
-    double result;
-
+    
+    double result = 0;
+    
     switch (op) {
       case 1:
-        result = addition(num1, num2);
-        break;
-
+      result = addition(num1, num2);
+      break;
+      
       case 2:
-        result = subtraction(num1, num2);
-        break;
+      result = subtraction(num1, num2);
+      break;
       case 3:
-        result = multiplication(num1, num2);
-        break;
+      result = multiplication(num1, num2);
+      break;
       case 4:
+      if (num2 == 0) {
+        System.out.println("divisor cannot be 0");
+        return;
+      } else {
         result = division(num1, num2);
-        break;
+      }
+      break;
       case 5:
+      if (num2 == 0) {
+        System.out.println("divisor cannot be 0");
+        return;
+      } else {
         result = remainder(num1, num2);
-        break;
+      }
+      break;
     }
+    
+    System.out.println("The result is: " + result);
+    sc.close();
   }
 }
