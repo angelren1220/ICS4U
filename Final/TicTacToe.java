45414f481ff6a4e97307761f141135744a555fb6
Linuxe <angelren1220@gmail.com>
2023-12-09 14:45:58 -0500

    add starter code

---
+++ Final/TicTacToe.java
@@ -0,0 +1,32 @@
+package Final;
+public class TicTacToe {
+  public static void main(String args[]) {
+    ticTacToe();
+  }
+
+  public static void ticTacToe() {
+    /*
+     * This is where the all the logic of the game would start if you were coding a
+     * full game.
+     */
+
+    /*
+     * You have to code the method printBoard that can then be called
+     * from here. The methods signature (including return type and
+     * parameters) and body will depend on your code design and how you * have
+     * decided to store your board. How the printed board looks also * depends on
+     * your design.
+     * Change the call to printBoard() below to match your method
+     * 
+     * 
+     * 
+     * 
+     * signature once you are done coding it.
+     * Hint - You probably want to be using an array.
+     */
+    printBoard();
+
+  }
+}
+
+/* Co de your printBoard method here! */
