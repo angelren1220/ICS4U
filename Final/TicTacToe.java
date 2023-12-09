cc4c2ef965e0198869027c54cf4a079242b70f54
Linuxe <angelren1220@gmail.com>
2023-12-09 15:37:19 -0500

    add initializeBoard

--- Final/TicTacToe.java
+++ Final/TicTacToe.java
@@ -1,35 +1,31 @@
 package Final;
 
 public class TicTacToe {
-      
+
   private static char[][] board = new char[3][3];
-  
+
   public static void main(String args[]) {
+    initializeBoard();
     ticTacToe();
   }
 
   public static void ticTacToe() {
-    /*
-     * This is where the all the logic of the game would start if you were coding a
-     * full game.
-     */
-    /*
-     * You have to code the method printBoard that can then be called
-     * from here. The methods signature (including return type and
-     * parameters) and body will depend on your code design and how you * have
-     * decided to store your board. How the printed board looks also * depends on
-     * your design.
-     * Change the call to printBoard() below to match your method
-     * 
-     * 
-     * 
-     * 
-     * signature once you are done coding it.
-     * Hint - You probably want to be using an array.
-     */
 
     printBoard();
 
+  }
+
+  /*
+   * Method initializeBoard
+   */
+
+  public static void initializeBoard() {
+    // Fill the board with spaces indicating empty positions
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        board[i][j] = ' ';
+      }
+    }
   }
 
   /*
@@ -52,4 +48,5 @@ public class TicTacToe {
       }
     }
   }
+
 }
